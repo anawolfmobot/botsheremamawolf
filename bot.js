ecbeac2de4c9afdb2733d5fe01900edd8d70bdd8
anawolfmobot <42562455+anawolfmobot@users.noreply.github.com>
2018-08-29 20:49:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,11 +12,13 @@ client.on('ready', () => {
 
 client.on("guildMemberAdd", member => {
     timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
-  let words = [`****
-سلام عليكم :heart: 
- مليت وانت تلعب لوحدك فورتنايت وتبغا تعرف كل تسريبات اللعبه و تبغا تشتري فيباكس وما معاك فيزا تبغا تعرف كل معلومات فورتنايت
-السيرفر دا يتيح لك كل شئ ادخل ما تتردد
-https://discord.gg/kBbZ5nU
+  let words = [`**
+سيرفر فورتنايت
+تبغا تعرف كل تسريبات فورتنايت
+تبغا تشوف الايتيم شوب حق فورتنايت بدون ما تفتح العبه
+تبغا تعرف كل شي جديد في فورتنايت
+تبغا لعب وفله وضحك السيرفر دا يضمنلك كل شي
+[ https://discord.gg/4ubEVQN ]
 **`]
   setTimeout(() =>{
   member.createDM().then(function (channel) {
@@ -28,13 +30,15 @@ https://discord.gg/kBbZ5nU
  
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-سلام عليكم :heart: 
- مليت وانت تلعب لوحدك فورتنايت وتبغا تعرف كل تسريبات اللعبه و تبغا تشتري فيباكس وما معاك فيزا تبغا تعرف كل معلومات فورتنايت
-السيرفر دا يتيح لك كل شئ ادخل ما تتردد
-https://discord.gg/kBbZ5nU
+  return channel.send(`**
+سيرفر فورتنايت
+تبغا تعرف كل تسريبات فورتنايت
+تبغا تشوف الايتيم شوب حق فورتنايت بدون ما تفتح العبه
+تبغا تعرف كل شي جديد في فورتنايت
+تبغا لعب وفله وضحك السيرفر دا يضمنلك كل شي
+[ https://discord.gg/4ubEVQN ]
 **`) 
 }).catch(console.error)
 })
+
 another.login(process.env.BOT_TOKEN);
